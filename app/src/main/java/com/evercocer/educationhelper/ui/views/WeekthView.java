8be81a4dfd386a1e38ab72c1ad291abb4a788509
evercocer <evercocer@gmail.com>
2021-04-06 23:06:45 +0800

    优化WeekthView

--- app/src/main/java/com/evercocer/educationhelper/ui/views/WeekthView.java
+++ app/src/main/java/com/evercocer/educationhelper/ui/views/WeekthView.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 
 public class WeekthView extends View {
     private String weekTH = "null";
+    private String sx = "";
     private Paint mPaint;
 
     private int mWidth;
@@ -59,14 +60,36 @@ public class WeekthView extends View {
         mPaint.setColor(Color.BLUE);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setTextSize(60);
+        mPaint.setTextSize(70);
         mPaint.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
-        canvas.drawText(weekTH, getWidth() / 5, getMeasuredHeight()-20, mPaint);
+        canvas.drawText(weekTH, getWidth() / 6, getMeasuredHeight()-20, mPaint);
 
         mPaint.setTextSize(20);
         mPaint.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-
-        canvas.drawText("th",getWidth()/4+35,getHeight()-20,mPaint);
+        switch (weekTH){
+            case "1":
+                sx = "st";
+                break;
+            case "2":
+                sx = "nd";
+                break;
+            case "3":
+                sx = "rd";
+                break;
+            case "11":
+                sx = "st";
+                break;
+            case "12":
+                sx = "nd";
+                break;
+            case "13":
+                sx = "rd";
+                break;
+            default:
+                sx = "th";
+                break;
+        }
+        canvas.drawText(sx,getWidth()/4+35,getHeight()-20,mPaint);
     }
 
     public String getWeekTH() {
